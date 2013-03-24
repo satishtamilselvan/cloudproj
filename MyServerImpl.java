178201d7d39156f686ac3da8b7e28b2c6318e555
satishtamilselvan <satishtamilselvan@hotmail.com>
2013-03-24 17:24:19 -0300

    Create MyServerImpl.java

---
+++ MyServerImpl.java
@@ -0,0 +1,125 @@
+ import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+ import java.rmi.server.UnicastRemoteObject;
+import java.util.Hashtable;
+ import java.util.Vector;
+ import javax.swing.Action;
+ import javax.swing.Timer;
+ 
+ public class MyServerImpl extends UnicastRemoteObject
+   implements ActionListener
+ {
+ Timer t1;
+Timer t2;
+Timer t3;
+   static Boolean temp1;
+   static Boolean temp2;
+   static Boolean temp3;
+   String userpro;
+    Hashtable pro;
+  
+    public MyServerImpl()
+      throws RemoteException
+    {
+    }
+  
+    public static void main(String[] args)
+    {
+         throw new Error("Unresolved compilation problems: \n\tmw cannot be resolved\n\tMinWin cannot be resolved to a type\n\tmw cannot be resolved\n\tmw cannot be resolved\n\tmw cannot be resolved\n\tmw cannot be resolved\n\tmw cannot be resolved\n");
+    }
+  
+    public String getpro(String proid)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tmw1 cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public int sendPacket(Vector vector, int i, String s, int j, int k)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tmw1 cannot be resolved\n\tmw1 cannot be resolved\n");
+    }
+  
+    public String getConnect(String s, String s1, int i, int j, String s2, String os)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tmw1 cannot be resolved\n\tmw cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tmw1 cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public String getagent(String s)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public void addHost(String s)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public String getipaddress()
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problem: \n");
+    }
+  
+    public Action LoadAction1()
+    {
+         throw new Error("Unresolved compilation problem: \n");
+    }
+  
+    public Action LoadAction2()
+    {
+         throw new Error("Unresolved compilation problem: \n");
+    }
+  
+    public Action LoadAction3()
+    {
+         throw new Error("Unresolved compilation problem: \n");
+    }
+  
+    public void actionPerformed(ActionEvent actionevent)
+    {
+         throw new Error("Unresolved compilation problem: \n");
+    }
+  
+    public String getConnect(String s, String s1)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public String getConnection(String s, String s1, String s2)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public void warningDisplay(String s, String s1)
+      throws RemoteException
+   {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public void warningDisplay1(String s, String s1)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public void delHost(String s)
+      throws RemoteException
+    {
+         throw new Error("Unresolved compilation problems: \n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  
+    public String attackTypes(String type, String ip, String date)
+      throws RemoteException
+    {
+          throw new Error("Unresolved compilation problems: \n\tmw1 cannot be resolved\n\tMinWin cannot be resolved\n");
+    }
+  }
+
+  
