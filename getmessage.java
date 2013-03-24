34bf2c41c61977005f91a41b4875d38d9e8742b2
satishtamilselvan <satishtamilselvan@hotmail.com>
2013-03-24 17:14:42 -0300

    Create getmessage.java

---
+++ getmessage.java
@@ -0,0 +1,13 @@
+ import java.io.InputStream;
+ import java.io.PrintStream;
+ 
+ class GetMessage extends Thread
+ {
+   PrintStream out1;
+ InputStream in1;
+
+   public void run()
+   {
+   throw new Error("Unresolved compilation problems: \n\tstream1 cannot be resolved\n\tStreamConnection cannot be resolved to a type\n\tConnector cannot be resolved\n\tstream1 cannot be resolved\n\tstream1 cannot be resolved\n\tAlert cannot be resolved to a type\n\tAlert cannot be resolved to a type\n\tAlert cannot be resolved\n\tAlertType cannot be resolved\n\tclient.display cannot be resolved\n");
+   }
+ }
