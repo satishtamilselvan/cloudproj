9d2425e80e5fd9ed3719b73e4f787daf988e8f05
satishtamilselvan <satishtamilselvan@hotmail.com>
2013-03-24 17:17:10 -0300

    Create LoginDialog$2.java

---
+++ LoginDialog$2.java
@@ -0,0 +1,15 @@
+ import java.awt.event.ActionEvent;
+ import java.awt.event.ActionListener;
+ 
+ final class LoginDialog$2
+   implements ActionListener
+ {
+   LoginDialog$2(LoginDialog paramLoginDialog)
+ {
+ }
+ 
+   public void actionPerformed(ActionEvent paramActionEvent)
+   {
+ this.this$0.jButClear_actionPerformed(paramActionEvent);
+   }
+ }
